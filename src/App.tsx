c0e7516ef8956c74908402ae354ec1d3561333e5
warlock <warlock@roadtowizardry.com>
2022-06-15 08:17:04 -0500

    working baseline

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
 
-function App() {
+const yeet = {
+  fire: ''
+};
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+  const options = {
+    eventType: 'keydown',
+    keystrokeDelay: 400
+  };
+
+  keyMapper([updateBackground, updateInputKeys], options);
+});
+
+function keyMapper(callbackList: any, options: any) {
+  const delay = hasProperty('keystrokeDelay', options) && options.keystrokeDelay >= 300 && options.keystrokeDelay;
+  const keystrokeDelay = delay || 1000;
+  const eventType = hasProperty('eventType', options) && options.eventType || 'keydown';
+
+  let state = {
+    buffer: [],
+    lastKeyTime: Date.now()
+  };
+
+  document.addEventListener(eventType, event => {
+    event.preventDefault()
+    const key = event.key;
+    const currentTime = Date.now();
+    let buffer:any = [];
+
+    if (currentTime - state.lastKeyTime > keystrokeDelay) {
+      buffer = [key];
+    } else {
+      buffer = [...state.buffer, key];
+    }
+
+    state = {buffer: buffer, lastKeyTime: currentTime};
+
+    callbackList.forEach((callback: (arg0: any) => any) => callback(buffer));
+  });
+
+  function hasProperty(property:any, object:any) {
+    return object && object.hasOwnProperty(property);
+  }
+}
+
+function updateBackground(keySequence:any) {
+  const validKeys = keySequence.every((key: string) => !isNaN(parseInt(key)) || key.toLowerCase() !== key.toUpperCase());
+  // console.log(validKeys, "Yeet!")
+}
+
+function updateInputKeys(keySequence: any) {
+  const userInput = keySequence.join('').toLowerCase();
+  const keySequences = {
+    'meta': 'Command Pressed',
+    'shiftshift': 'Search Everywhere in all the things',
+
+  };
+  const userInputDisplay = document.querySelector('#user_input');
+  // @ts-ignore
+  userInputDisplay.textContent = userInput;
+  console.log(userInput)
+
+  yeet.fire =userInput // jank real jank
+
+  const inputMessage = document.querySelector('#cheat_message');
+  // @ts-ignore
+  inputMessage.textContent = keySequences[userInput] || 'Nothing';
+}
+
+function App(){
+  const [dankmeme] = useState(yeet.fire)
+  const [viewCounter, setViewCounter] = useState(0)
+  useEffect(()=> {
+    setInterval(()=> {
+      setViewCounter(viewCounter+1)
+    }, 100)
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+      <aside className="sidebar">
+        <h1 className="instructions">quickly type a key Ma sequences to do things:</h1>
+        <ul className="list">
+          {/*// flexbox add "Legend*/}
+          {/*// ⇧ - Shift⌥ - Alt⌃ - Control⌘ - Command↵ - Enter"*/}
+          <li className="list__item">Command</li>
+          <li className="list__item">Shift Shift</li>
+        </ul>
+        <div className="user_input">
+          <p className="user_input__label">You entered:</p>
+          <p id="user_input" className="user_input__value"/>
+        </div>
+        <div id="cheat_block" className="cheat_block">
+          <p>You got:</p>
+          <p id="cheat_message" className="cheat_message">nothing yet</p>
+        </div>
+      </aside>
+      {dankmeme}
     </div>
   );
 }
---
+++ src/components/data.ts
@@ -0,0 +1,19 @@
+const data = [
+        {
+            questionText:'Search Everywhere',
+            answer:
+                { answerText: 'shiftshift', isCorrect: true },
+        },
+        {
+            questionText: 'Find Action',
+            answer:
+                { answerText: 'shiftmetaA', isCorrect: true },
+        },
+        {
+            questionText: 'Show Context Actions',
+            answer:
+                { answerText: 'altenter', isCorrect: true },
+        },
+    ];
+
+export default data;
